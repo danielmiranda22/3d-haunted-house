788ecd29ace5f55e508a9437fe146a25526d6309
Daniel Oliveira <danmiranda227@gmail.com>
2026-09-08 15:20:30 +0100

    chore: adds three.js built in sky

--- src/script.js
+++ src/script.js
@@ -2,7 +2,7 @@ import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 import { Timer } from "three/addons/misc/Timer.js";
 import GUI from "lil-gui";
-import { add, element } from "three/tsl";
+import { Sky } from "three/examples/jsm/Addons.js";
 
 /**
  * Base
@@ -418,6 +418,18 @@ ghost2.shadow.camera.far = 10;
 ghost3.shadow.mapSize.width = 256;
 ghost3.shadow.mapSize.height = 256;
 ghost3.shadow.camera.far = 10;
+
+/**
+ * Sky
+ */
+const sky = new Sky();
+sky.material.uniforms["turbidity"].value = 10;
+sky.material.uniforms["rayleigh"].value = 3;
+sky.material.uniforms["mieCoefficient"].value = 0.1;
+sky.material.uniforms["mieDirectionalG"].value = 0.95;
+sky.material.uniforms["sunPosition"].value.set(0.3, -0.038, -0.95);
+sky.scale.set(100, 100, 100);
+scene.add(sky);
 
 /**
  * Animate
